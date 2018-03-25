60e41a39d9bb479138cc1272ba9e51e0db803384
Mads <mkrn@itu.dk>
2018-03-25 13:22:15 +0200

    added random

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,13 @@
     <input type="text" class="search" v-model="searchInput" @keyup.enter="fetchGif(searchInput)">
     <div >
       <button class="search-btn" @click="fetchGif(searchInput)">Search!</button>
-      <button @click="gifLinks=[]">Clear</button>
+      <button @click="fetchRandom()">Random</button>
+      <button @click="clear()">Clear</button>
       </div>
+        <p v-if="isLoading">Loading... </p>
       <div class="gif-container">
-        <img v-for="gif in gifLinks" :src="gif" alt="" :key="gif">
+        <img class="random-img" v-if="isRandom" :src="randomGif" alt="">
+        <img class="searched-img" v-for="gif in gifLinks" :src="gif" alt="" :key="gif">
       </div>
     </div>
   </div>
@@ -23,29 +26,69 @@ export default {
     return {
       msg: "Super Awesome Gif Searcher",
       apiKey: "&api_key=lhFQfxgJLjCyauVDsuPp9tExkQoSN6gf",
-      apiUrl: "https://api.giphy.com/v1/gifs/search?",
+      searchUrl: "https://api.giphy.com/v1/gifs/search?",
+      randomUrl: "https://api.giphy.com/v1/gifs/random?",
       searchInput: "",
-      gifLinks: []
+      gifLinks: [],
+      isRandom: false,
+      randomGif: "",
+      isLoading: false
     };
   },
 
   methods: {
     fetchGif: function(searchTerm) {
-      this.gifLinks = [];
+      this.clear();
+      this.toggleLoading();
       let limit = 9;
-      const url = `${this.apiUrl + this.apiKey}&q=${searchTerm}&limit=${limit}`;
+      const url = `${this.searchUrl +
+        this.apiKey}&q=${searchTerm}&limit=${limit}`;
       fetch(url)
         .then(response => {
           return response.json();
         })
         .then(json => {
-          this.gifLinks = json.data
-            .map(gif => gif.id)
-            .map(gifId => `https://media.giphy.com/media/${gifId}/giphy.gif`);
+          this.isRandom = false;
+          this.buildGif(json);
+          this.toggleLoading();
         })
         .catch(function(err) {
           console.log("Fetch Error :-S", err);
         });
+      this.searchInput = "";
+    },
+    fetchRandom: function() {
+      this.clear();
+      this.toggleLoading();
+      const url = `${this.randomUrl + this.apiKey}`;
+      fetch(url)
+        .then(response => {
+          return response.json();
+        })
+        .then(json => {
+          this.isRandom = true;
+          this.buildGif(json);
+          this.toggleLoading();
+        });
+    },
+    buildGif: function(json) {
+      this.gifLinks = [];
+      if (this.isRandom) {
+        this.randomGif = `https://media.giphy.com/media/${
+          json.data.id
+        }/giphy.gif`;
+      } else {
+        this.gifLinks = json.data
+          .map(gif => gif.id)
+          .map(gifId => `https://media.giphy.com/media/${gifId}/giphy.gif`);
+      }
+    },
+    toggleLoading: function() {
+      this.isLoading = !this.isLoading;
+    },
+    clear: function() {
+      this.gifLinks = [];
+      this.randomGif = "";
     }
   }
 };
@@ -73,13 +116,20 @@ html {
   display: grid;
   grid-gap: 15px;
   grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
-  img {
+  .searched-img {
     height: 300px;
     width: 100%;
     object-fit: cover;
   }
   max-width: 1280px;
   margin: 0 auto;
+}
+
+.random-img {
+  margin: 0 auto;
+  // height: 300px;
+  // width: 50%;
+  // object-fit: cover;
 }
 
 h1,
