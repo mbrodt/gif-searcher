8d730f290b007fe66c542b9469d4eeb7dd7c4d94
Mads <mkrn@itu.dk>
2018-04-04 18:50:33 +0200

    moved scss into file

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import "./assets/css/style.scss";
 
 new Vue({
-  el: '#app',
+  el: "#app",
   render: h => h(App)
-})
+});
